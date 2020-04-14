49aa9983939d1089336581bcabf9154dd5bcc9a1
AshaR-246 <62698273+AshaR-246@users.noreply.github.com>
2020-04-14 18:22:42 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,43 @@
+var ball,locNode
+
+function setup(){
+    database=firebase.database();
+    createCanvas(500,500);
+    ball = createSprite(250,250,10,10);
+    ball.shapeColor = "red";
+    var locNode=database.ref("Ball/Positions");
+    locNode.on("value",readOp,showErr);
+}
+
+function draw(){
+    background("yellow");
+    if(keyDown(LEFT_ARROW)){
+        writePosition(-1,0);
+    }
+    else if(keyDown(RIGHT_ARROW)){
+        writePosition(1,0);
+    }
+    else if(keyDown(UP_ARROW)){
+        writePosition(0,-1);
+    }
+    else if(keyDown(DOWN_ARROW)){
+        writePosition(0,+1);
+    }
+    drawSprites();
+}
+
+function writePosition(x,y){
+    database.ref("Ball/Positions").set({
+        "x" : ball.x + x,
+        "y" : ball.y + y
+    })
+   
+}
+function readOp(data){
+    position=data.val();
+    ball.x=position.x;
+    ball.y=position.y;
+}
+function showErr(){
+    console.log("erorr");
+}
